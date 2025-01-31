84237350a6905c701974f86f5d6d6a470d98ad7d
Lukearmstrong2201 <lukearmstrong2201@hotmail.com>
2025-01-31 15:08:55 +0000

    Started Dashboard page implementing grid sections

--- src/App.jsx
+++ src/App.jsx
@@ -4,15 +4,20 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
 import Homepage from "./pages/HomePage";
 import Header from "./components/Header";
+import Dashboard from "./pages/Dashboard";
 
 function App() {
   return (
     <Router>
       <div className="app">
         <Header />
+        <main className="main-content">
+          <Routes>
+            <Route path="/" element={<Homepage />} />
+            <Route path="/dashboard" element={<Dashboard />} />
+          </Routes>
+        </main>
       </div>
-
-      <Homepage />
     </Router>
   );
 }
