1e4c8becf9a15b42cc24d629e397e066c98b6b60
Lukearmstrong2201 <lukearmstrong2201@hotmail.com>
2025-02-05 23:11:44 +0000

    Initialise API file Requesting ski resort data from RapidAPI

--- src/services/api.js
+++ src/services/api.js
@@ -1,4 +1,4 @@
-const API_KEY = "Enter RapidApi API Key"; //Enter 
+const API_KEY = "10249de295msh286762ddd8dd817p10df24jsn84a15d6b41d0";
 const BASE_URL = "https://ski-resorts-and-conditions.p.rapidapi.com/v1/resort";
 let url = `${BASE_URL}/whistler-blackcomb`; //STATE NEEDS LIFTING FROM RESORTS PAGE (Replace Resort with formatted string)
 
@@ -14,9 +14,16 @@ const options = {
 export const getSkiResortConditions = async () => {
   try {
     const response = await fetch(url, options);
-    const result = await response.json();
-    console.log(result);
+    const data = await data.json();
+    return data.conditions;
   } catch (error) {
     console.error("Error fetching ski resort data:", error);
   }
 };
+
+export const getSkiResort = async () => {
+  const response = await fetch(url, options);
+  const data = await data.json();
+  return data;
+};
+getSkiResort();
